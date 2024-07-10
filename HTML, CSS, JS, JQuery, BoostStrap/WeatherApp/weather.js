8baa2d7ab80fe3764c5bd042caff1ec56d451607
Pannu YN <pannuyannaing.fw@gmail.com>
2024-07-10 23:28:12 +0630

    weather-app updated

--- HTML, CSS, JS, JQuery, BoostStrap/WeatherApp/weather.js
+++ HTML, CSS, JS, JQuery, BoostStrap/WeatherApp/weather.js
@@ -18,17 +18,35 @@ async function showData() {
     const selected_city = comboBox.value;
     const data = await fetchData(selected_city, "forecast.json");
     console.log(data);
+
+
+    // Access the `hour` array
+    const hours = data.forecast.forecastday[0].hour;
+    const currentTime = '2024-07-10 20:00';
+    const currentWeather = hours.find(hour => hour.time === currentTime);
+    console.log(currentWeather.condition.text);
+
     const condition = document.createElement('h3');
     condition.classList.add("condition-css");
     condition.classList.add("display-5");
-    condition.textContent = data.current.condition.text
+    condition.textContent = currentWeather.condition.text;
     const section = document.querySelector('#weather-section');
-
+    const hourBtns = document.createElement('div');
+    hourBtns.classList.add("d-flex");
+    hourBtns.classList.add("flex-wrap");
 
     //remove previous image inserted in the modal before inserting another
     while (section.firstChild) { //till there is a child
         section.removeChild(section.firstChild); //remove that child
     }
+
+    hours.map(hour => {
+        const btn = document.createElement('button');
+        btn.classList.add("col-1");
+        btn.textContent = hour.time.split(" ")[1];
+        hourBtns.appendChild(btn);
+    })
+    section.appendChild(hourBtns);
     section.appendChild(condition);
 }
 
